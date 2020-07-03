639fd97ab13bb136dea8b038fa819421264f122a
sleepyboy <sleepyboy123@hotmail.com>
2020-07-03 11:00:06 +1000

    Changed web to client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import socketIOClient from 'socket.io-client';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+
+}
+
+export default App;
