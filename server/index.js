e00cbcf5eb1f69a107ef791b13292a0c5de388d1
sleepyboy <sleepyboy123@hotmail.com>
2020-07-03 17:56:26 +1000

    Final Commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -11,6 +12,9 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router)
+app.use(cors());
 
 io.on('connection', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
@@ -45,7 +49,5 @@ io.on('connection', (socket) => {
         }
     })
 })
-
-app.use(router)
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
